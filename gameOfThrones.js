9d21e95db719322f618b2d5fcbbe798b1b2ea6d5
Shipalan <Shipalan678@gmail.com>
2022-05-25 12:26:17 -0600

    Pre battle2

--- gameOfThrones.js
+++ gameOfThrones.js
@@ -3,6 +3,8 @@ let jamieLannisterAttack = 35;
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister");
-} else {
+} else if (jonSnowAttack < jamieLannisterAttack) {
     console.log("Jamie Lannister has better attack than John Snow");
+} else {
+    console.log('jon snow and jamie lannister have the same attack');
 }
